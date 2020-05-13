0e97a3c7cdc84cfbe69b4cdf1744ac1684a6ad90
Yogiraj <yogiraj.kadikar@gmail.com>
2020-05-13 18:34:53 +0530

    new function added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from collections import Counter
 
-#Please enter your credentials from twitter site
+#Please enter your credentials from twitter developer site
 ACCESS_TOKEN = ""
 ACCESS_TOKEN_SECRET = ""
 CONSUMER_KEY = ""
@@ -12,16 +12,16 @@ CONSUMER_SECRET = ""
 auth=twitter.oauth.OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
 twitter_api =twitter.Twitter(auth=auth)
-#print(twitter_api)
 
+#initializing flask
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE-URI']='postgresql://postgres:yogikadikar@localhost/trending_hastags'
-db = SQLAlchemy(app)
 
+#main webpage of app
 @app.route("/")
 def index():
     return render_template("index.html")
 
+#function to add a WOEid
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
     if request.method == 'POST':
@@ -32,22 +32,20 @@ def add():
         return render_template("index.html", _trends_set= _trends_set)
     return render_template("index.html", )
 
+#counting how frequntly was hashtag used
 @app.route('/frequency', methods = ['GET','POST'])
-def frequncy(): #counting how frequntly was hashtag used
+def frequncy(): 
     if request.method == 'POST':
         q = request.form['hashtag']
         count = 100
         search_results = twitter_api.search.tweets(q=q, count=count)
         statuses = search_results['statuses']
         for _ in range(5):
-            #print ("Length of statuses", len(statuses))
             try:
                 next_results = search_results['search_metadata']['next_results']
             except KeyError: # No more results when next_results doesn't exist
                 break
-        
-    # Create a dictionary from next_results, which has the following form:
-    # ?max_id=313519052523986943&q=NCAA&include_entities=1
+
         kwargs = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
     
         search_results = twitter_api.search.tweets(**kwargs)
@@ -62,6 +60,49 @@ def frequncy(): #counting how frequntly was hashtag used
         return render_template("index.html", frequent = frequent )
     return render_template("index.html", )
 
+#Function to get top 10 tweets for a particular hashtag
+@app.route('/query', methods = ['GET','POST'])
+def query():
+    if request.method == 'POST':
+        q = request.form['hashtag_n']
+        def twitter_search(twitter_api, q, max_results=200, **kw):
+            search_results = twitter_api.search.tweets(q=q, count=100, **kw)
+        
+            statuses = search_results['statuses']
+            max_results = min(1000, max_results)
+        
+            for _ in range(10): # 10*100 = 1000
+                try:
+                    next_results = search_results['search_metadata']['next_results']
+                except KeyError: # No more results when next_results doesn't exist
+                    break
+
+                kwargs = dict([ kv.split('=') 
+                                for kv in next_results[1:].split("&") ])
+                
+                search_results = twitter_api.search.tweets(**kwargs)
+                statuses += search_results['statuses']
+                
+                if len(statuses) > max_results: 
+                    break
+                    
+            return statuses
+        def find_popular_tweets(twitter_api, statuses, retweet_threshold=3):
+            return [ status
+                for status in statuses 
+                    if status['retweet_count'] > retweet_threshold ] 
+
+        search_results = twitter_search(twitter_api, q, max_results=200)
+        popular_tweets = find_popular_tweets(twitter_api, search_results)
+        a=0
+        tweet=None
+        twit=[]
+        for tweet in popular_tweets:
+            if a<10:
+                a=a+1
+                twit.append(tweet['text'])
+        return render_template("index.html",  twit= twit )
+    return render_template("index.html", )
 
 
 if __name__ == "__main__":
